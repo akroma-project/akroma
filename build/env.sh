9cb7595b85394f3ce4ab85a6c45086362ebf9940
detroitpro <eric@polerecky.com>
2018-06-12 20:31:39 -0400

    build:fix github path

--- build/env.sh
+++ build/env.sh
@@ -10,11 +10,11 @@ fi
 # Create fake Go workspace if it doesn't exist yet.
 workspace="$PWD/build/_workspace"
 root="$PWD"
-ethdir="$workspace/src/github.com/ethereum"
-if [ ! -L "$ethdir/go-ethereum" ]; then
+ethdir="$workspace/src/github.com/akroma-project"
+if [ ! -L "$ethdir/akroma" ]; then
     mkdir -p "$ethdir"
     cd "$ethdir"
-    ln -s ../../../../../. go-ethereum
+    ln -s ../../../../../. akroma
     cd "$root"
 fi
 
@@ -23,8 +23,8 @@ GOPATH="$workspace"
 export GOPATH
 
 # Run the command inside the workspace.
-cd "$ethdir/go-ethereum"
-PWD="$ethdir/go-ethereum"
+cd "$ethdir/akroma"
+PWD="$ethdir/akroma"
 
 # Launch the arguments with the configured environment.
 exec "$@"
