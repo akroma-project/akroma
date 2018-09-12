17d3a9ac46215d88d06071127b7e9a04e10fd91c
Viktor Trón <viktor.tron@gmail.com>
2018-09-12 11:24:56 +0200

    Kademlia refactor  (#17641)
    * swarm/network: simplify kademlia/hive; rid interfaces
    
    * swarm, swarm/network/stream, swarm/netork/simulations,, swarm/pss: adapt to new Kad API
    
    * swarm/network: minor changes re review; add missing lock to NeighbourhoodDepthC

--- swarm/network_test.go
+++ swarm/network_test.go
@@ -34,7 +34,6 @@ import (
 	"github.com/akroma-project/akroma/p2p/discover"
 	"github.com/akroma-project/akroma/p2p/simulations/adapters"
 	"github.com/akroma-project/akroma/swarm/api"
-	"github.com/akroma-project/akroma/swarm/network"
 	"github.com/akroma-project/akroma/swarm/network/simulation"
 	"github.com/akroma-project/akroma/swarm/storage"
 	colorable "github.com/mattn/go-colorable"
@@ -293,7 +292,7 @@ func testSwarmNetwork(t *testing.T, o *testSwarmNetworkOptions, steps ...testSwa
 			if err != nil {
 				return nil, cleanup, err
 			}
-			bucket.Store(simulation.BucketKeyKademlia, swarm.bzz.Hive.Overlay.(*network.Kademlia))
+			bucket.Store(simulation.BucketKeyKademlia, swarm.bzz.Hive.Kademlia)
 			log.Info("new swarm", "bzzKey", config.BzzKey, "baseAddr", fmt.Sprintf("%x", swarm.bzz.BaseAddr()))
 			return swarm, cleanup, nil
 		},
