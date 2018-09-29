b2502ebcf27511ee8e14eab1dae807eb8db94bad
HackyMiner <hackyminer@gmail.com>
2018-09-29 23:15:39 +0900

    internal/debug: support color terminal for cygwin/msys2 (#17740)
    - update go-colorable, go-isatty, go-runewidth packages
    - use go-isatty instead of log/term and remove log/term package

--- internal/debug/flags.go
+++ internal/debug/flags.go
@@ -25,11 +25,11 @@ import (
 	"runtime"
 
 	"github.com/akroma-project/akroma/log"
-	"github.com/akroma-project/akroma/log/term"
 	"github.com/akroma-project/akroma/metrics"
 	"github.com/akroma-project/akroma/metrics/exp"
 	"github.com/fjl/memsize/memsizeui"
 	colorable "github.com/mattn/go-colorable"
+	"github.com/mattn/go-isatty"
 	"gopkg.in/urfave/cli.v1"
 )
 
@@ -101,7 +101,7 @@ var (
 )
 
 func init() {
-	usecolor := term.IsTty(os.Stderr.Fd()) && os.Getenv("TERM") != "dumb"
+	usecolor := (isatty.IsTerminal(os.Stderr.Fd()) || isatty.IsCygwinTerminal(os.Stderr.Fd())) && os.Getenv("TERM") != "dumb"
 	output := io.Writer(os.Stderr)
 	if usecolor {
 		output = colorable.NewColorableStderr()
