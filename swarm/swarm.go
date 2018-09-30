080046cf3f7295c04a14428657e637dc9f4e5c77
Javier Peletier <jm@epiclabs.io>
2018-09-30 09:51:11 +0200

    swarm/storage/feeds: Final package rename and moved files

--- swarm/swarm.go
+++ swarm/swarm.go
@@ -50,7 +50,7 @@ import (
 	"github.com/akroma-project/akroma/swarm/state"
 	"github.com/akroma-project/akroma/swarm/storage"
 	"github.com/akroma-project/akroma/swarm/storage/mock"
-	"github.com/akroma-project/akroma/swarm/storage/mru"
+	"github.com/akroma-project/akroma/swarm/storage/feeds"
 	"github.com/akroma-project/akroma/swarm/tracing"
 )
 
@@ -186,10 +186,10 @@ func NewSwarm(config *api.Config, mockStore *mock.NodeStore) (self *Swarm, err e
 	// Swarm Hash Merklised Chunking for Arbitrary-length Document/File storage
 	self.fileStore = storage.NewFileStore(self.netStore, self.config.FileStoreParams)
 
-	var feedsHandler *mru.Handler
-	fhParams := &mru.HandlerParams{}
+	var feedsHandler *feeds.Handler
+	fhParams := &feeds.HandlerParams{}
 
-	feedsHandler = mru.NewHandler(fhParams)
+	feedsHandler = feeds.NewHandler(fhParams)
 	feedsHandler.SetStore(self.netStore)
 
 	lstore.Validators = []storage.ChunkValidator{
