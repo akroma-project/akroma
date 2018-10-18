16431e0713da69994066951f0b6a8a21d6ea2256
Simon Jentzsch <simon@slock.it>
2018-10-18 21:41:22 +0200

    EIP-1186 eth_getProof (#17737)
    * first impl of eth_getProof
    
    * fixed docu
    
    * added comments and refactored based on comments from holiman
    
    * created structs
    
    * handle errors correctly
    
    * change Value to *hexutil.Big in order to have the same output as parity
    
    * use ProofList as return type

--- core/state/statedb.go
+++ core/state/statedb.go
@@ -18,6 +18,7 @@
 package state
 
 import (
+	"errors"
 	"fmt"
 	"math/big"
 	"sort"
@@ -25,6 +26,7 @@ import (
 
 	"github.com/akroma-project/akroma/common"
 	"github.com/akroma-project/akroma/core/types"
+	"github.com/akroma-project/akroma/core/vm"
 	"github.com/akroma-project/akroma/crypto"
 	"github.com/akroma-project/akroma/log"
 	"github.com/akroma-project/akroma/rlp"
@@ -254,6 +256,24 @@ func (self *StateDB) GetState(addr common.Address, hash common.Hash) common.Hash
 		return stateObject.GetState(self.db, hash)
 	}
 	return common.Hash{}
+}
+
+// GetProof returns the MerkleProof for a given Account
+func (self *StateDB) GetProof(a common.Address) (vm.ProofList, error) {
+	var proof vm.ProofList
+	err := self.trie.Prove(crypto.Keccak256(a.Bytes()), 0, &proof)
+	return proof, err
+}
+
+// GetProof returns the StorageProof for given key
+func (self *StateDB) GetStorageProof(a common.Address, key common.Hash) (vm.ProofList, error) {
+	var proof vm.ProofList
+	trie := self.StorageTrie(a)
+	if trie == nil {
+		return proof, errors.New("storage trie for requested address does not exist")
+	}
+	err := trie.Prove(crypto.Keccak256(key.Bytes()), 0, &proof)
+	return proof, err
 }
 
 // GetCommittedState retrieves a value from the given account's committed storage trie.
