fe7a1e1335695e8028bc2a288c924b952710b596
Sean Jenkins <sean@betterservers.com>
2018-12-24 10:53:16 -0700

    Fix import URIs

--- p2p/simulations/test.go
+++ p2p/simulations/test.go
@@ -3,10 +3,10 @@ package simulations
 import (
 	"testing"
 
-	"github.com/ethereum/go-ethereum/p2p"
-	"github.com/ethereum/go-ethereum/p2p/enode"
-	"github.com/ethereum/go-ethereum/p2p/enr"
-	"github.com/ethereum/go-ethereum/rpc"
+	"github.com/akroma-project/akroma/p2p"
+	"github.com/akroma-project/akroma/p2p/enode"
+	"github.com/akroma-project/akroma/p2p/enr"
+	"github.com/akroma-project/akroma/rpc"
 )
 
 // NoopService is the service that does not do anything
--- swarm/pss/forwarding_test.go
+++ swarm/pss/forwarding_test.go
@@ -6,13 +6,13 @@ import (
 	"testing"
 	"time"
 
-	"github.com/ethereum/go-ethereum/crypto"
-	"github.com/ethereum/go-ethereum/p2p"
-	"github.com/ethereum/go-ethereum/p2p/enode"
-	"github.com/ethereum/go-ethereum/p2p/protocols"
-	"github.com/ethereum/go-ethereum/swarm/network"
-	"github.com/ethereum/go-ethereum/swarm/pot"
-	whisper "github.com/ethereum/go-ethereum/whisper/whisperv5"
+	"github.com/akroma-project/akroma/crypto"
+	"github.com/akroma-project/akroma/p2p"
+	"github.com/akroma-project/akroma/p2p/enode"
+	"github.com/akroma-project/akroma/p2p/protocols"
+	"github.com/akroma-project/akroma/swarm/network"
+	"github.com/akroma-project/akroma/swarm/pot"
+	whisper "github.com/akroma-project/akroma/whisper/whisperv5"
 )
 
 type testCase struct {
