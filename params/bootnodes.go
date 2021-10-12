08ab0bc1eeb05ea4f831d42bee5f0d19ee2c53f0
Eric Polerecky <eric@polerecky.com>
2021-10-11 21:46:24 -0400

    fix: update bootnodes

--- params/bootnodes.go
+++ params/bootnodes.go
@@ -20,7 +20,6 @@ package params
 // the main Akroma network.
 var MainnetBootnodes = []string{
 	"enode://e9fb6b5bfa04bbfe134e2b34d761d669defe68a2ce311a59231d524ccd003b6f833703546a58dc9a6791939e63226fa04beaa02a6d4fc8aa603fff537bf9642d@137.184.120.92:30303",
-	"enode://dc4fc0bb21ce46a048d51e0e4722a79918d2a671c24885d53d2bea815b32178cb28d3544170fe0404095d9fe8dc5770814300effba773854420aad563138eceb@192.99.244.230:30303",
 }
 
 // TestnetBootnodes are the enode URLs of the P2P bootstrap nodes running on the
