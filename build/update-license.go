1fd0e8e4258c475d6f1a2c259ef1348746b4278a
detroitpro <eric@polerecky.com>
2021-11-15 21:14:29 -0500

    chore: remove unused contracts

--- build/update-license.go
+++ build/update-license.go
@@ -14,6 +14,7 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
 
+//go:build none
 // +build none
 
 /*
@@ -72,8 +73,6 @@ var (
 		"internal/jsre/deps",
 		"log/",
 		"common/bitutil/bitutil",
-		// don't license generated files
-		"contracts/chequebook/contract/code.go",
 	}
 
 	// paths with this prefix are licensed as GPL. all other files are LGPL.
