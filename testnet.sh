f1e51f57663bc80b5d3ffc0cced8e60ec888d604
detroitpro <eric@polerecky.com>
2021-12-18 14:58:34 -0500

    chore: testnet script

--- testnet.sh
+++ testnet.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 rm -fr ~/.akromatests/testnet/geth/chaindata
 
-./build/bin/geth --datadir ~/.akromatests/testnet/ --testnet --rpcport 8544 --rpc --port 30302 --nodiscover --nat extip:127.0.0.1 --mine --miner.threads 1
+./build/bin/geth --datadir ~/.akromatests/testnet/ --testnet --rpcport 8544 --rpc --port 30302 --nodiscover --nat extip:127.0.0.1 --mine --miner.threads 1 --miner.etherbase 0x96ecaaf5b1669d374406f7e22c217fc839ca6bd1 --miner.extradata akroma.org-miner
